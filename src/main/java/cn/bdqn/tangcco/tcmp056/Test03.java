8e8fff73d0af8f64570757c4a50ac6026c06b4fc
javaGCH <794147680@qq.com>
2017-08-05 18:22:13 +0800

    Initial commit

---
+++ src/main/java/cn/bdqn/tangcco/tcmp056/Test03.java
@@ -0,0 +1,13 @@
+package cn.bdqn.tangcco.tcmp056;
+
+import org.junit.Test;
+
+/**
+ * Created by 79414 on 2017/8/5.
+ */
+public class Test03 {
+    @Test
+    public void testHelloWorld() {
+        System.out.println("Hello world!");
+    }
+}
